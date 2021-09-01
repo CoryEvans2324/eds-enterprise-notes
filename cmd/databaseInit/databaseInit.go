5f108592d0b0c2a91348bff5b38e97349311c2d8
Cory Evans <cory.evans@mailbox.org>
2021-09-01 01:05:51 +0000

    Install documentation (#16)
    * initial install docs
    
    * go program to create example data

---
+++ cmd/databaseInit/databaseInit.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/CoryEvans2324/eds-enterprise-notes/config"
+	"github.com/CoryEvans2324/eds-enterprise-notes/database"
+)
+
+func init() {
+	cfgData, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	config.LoadConfig(cfgData)
+}
+
+func checkErrorNil(err error) {
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+}
+
+func main() {
+	cfg := config.Get()
+
+	err := database.CreateDatabaseManager(cfg.Database.DataSourceName())
+	checkErrorNil(err)
+
+	checkErrorNil(database.Mgr.DropUserTable())
+	checkErrorNil(database.Mgr.CreateUserTable())
+	checkErrorNil(database.Mgr.DropNoteTable())
+	checkErrorNil(database.Mgr.CreateNoteTable())
+	checkErrorNil(database.Mgr.DropPermissionTable())
+	checkErrorNil(database.Mgr.CreatePermissionTable())
+
+	createFakeUsers()
+	createFakeNotes()
+}
+
+func createFakeUsers() {
+	users := []string{
+		"cory",
+		"Alleen",
+		"Demeter",
+		"Bamby",
+		"Brenda",
+		"Ophelie",
+		"Tobe",
+		"Nada",
+		"Fey",
+		"Janeczka",
+		"Merissa",
+		"Nancy",
+		"Gavra",
+		"Jessika",
+		"Charisse",
+		"Wynn",
+		"Linnet",
+		"Verna",
+		"Cacilie",
+		"Moina",
+		"Ardyth",
+	}
+
+	for i := 0; i < len(users); i += 2 {
+		database.Mgr.CreateUser(users[i], "password123")
+	}
+}
+
+func createFakeNotes() {
+
+}
